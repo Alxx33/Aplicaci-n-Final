5d27589229e016814dcd101920231fb35d23d5c7
Alxx33 <30987675+Alxx33@users.noreply.github.com>
2018-11-26 16:28:23 -0600

    Add files via upload

---
+++ App Final/proyecto2A1/ItemCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCollectionViewCell.swift
+//  proyecto2A1
+//
+//  Created by Macbook on 11/26/18.
+//  Copyright © 2018 unam fca. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var dia: UILabel!
+    
+}
